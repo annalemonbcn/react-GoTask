947f80a81f81c23631158f535200ed0490b084b0
Anna Esteve <aesteve8@gmail.com>
2024-01-22 15:33:51 +0100

    ADD footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import Footer from "./components/Footer";
 import HomeView from "./views/HomeView";
 import WelcomeView from "./views/WelcomeView";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<WelcomeView />} />
-      <Route path="/tasks" element={<HomeView />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<WelcomeView />} />
+        <Route path="/tasks" element={<HomeView />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
